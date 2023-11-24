2133c19e54bbf80eb8fd0e2a2e4affcc7d5d1a7a
Sinaru Gunawardena <sinarug@gmail.com>
2023-11-24 23:57:37 +0000

    Handle CLI args

---
+++ lib/cv_builder/arg_parser.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+require 'optparse'
+
+module CvBuilder
+  module ArgParser
+    def self.parse!
+      options = {}
+
+      OptionParser.new do |parser|
+        parser.on("-c", "--cv-info INFO") do |cv_info|
+          p cv_info
+          options[:cv_info] = cv_info
+        end
+
+        parser.on("-o", "--output-path OUTPUT") do |output_path|
+          options[:output_path] = output_path
+        end
+      end.parse!
+
+      options
+    end
+  end
+end
