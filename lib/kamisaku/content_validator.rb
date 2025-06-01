7db83dbf598958e5c4652ba46e7e0975f7e61ba2
Sinaru Gunawardena <sinarug@gmail.com>
2025-06-01 12:12:33 +0100

    Extend ContentValidator to validate `contact` and `location` fields
    - Added validation logic for `contact` and nested `location` fields in `Kamisaku::ContentValidator`.
    - Implemented comprehensive test coverage for new validation rules.

--- lib/kamisaku/content_validator.rb
+++ lib/kamisaku/content_validator.rb
@@ -10,6 +10,7 @@ module Kamisaku
     def validate!
       validate_version
       validate_profile
+      validate_contact
     end
 
     private
@@ -37,6 +38,46 @@ module Kamisaku
         unless value.is_a?(String)
           raise Error, "Profile field '#{field}' must be a string"
         end
+      end
+    end
+
+    def validate_contact
+      raise Error, "Missing contact" unless data[:contact]
+      raise Error, "Contact must be a hash" unless data[:contact].is_a?(Hash)
+
+      allowed_fields = %i[github mobile linkedin website email location]
+      contact_fields = data[:contact].keys
+
+      unless contact_fields.all? { |field| allowed_fields.include?(field) }
+        raise Error, "Contact contains invalid fields"
+      end
+
+      allowed_fields.each do |field|
+        raise Error, "Contact missing required field '#{field}'" unless contact_fields.include?(field)
+      end
+
+      data[:contact].each do |field, value|
+        if field == :location
+          validate_location(value)
+        else
+          raise Error, "Contact field '#{field}' must be a string" unless value.is_a?(String)
+        end
+      end
+    end
+
+    def validate_location(location)
+      raise Error, "Location must be a hash" unless location.is_a?(Hash)
+
+      allowed_fields = %i[country city]
+      location_fields = location.keys
+
+      unless location_fields.all? { |field| allowed_fields.include?(field) }
+        raise Error, "Location contains invalid fields"
+      end
+
+      allowed_fields.each do |field|
+        raise Error, "Location missing required field '#{field}'" unless location_fields.include?(field)
+        raise Error, "Location field '#{field}' must be a string" unless location[field].is_a?(String)
       end
     end
   end
--- test/kamisaku/content_validator_test.rb
+++ test/kamisaku/content_validator_test.rb
@@ -9,6 +9,17 @@ module Kamisaku
           name: "Foo",
           title: "Some Job Title",
           about: "Some Job about text"
+        },
+        contact: {
+          github: "foobar",
+          mobile: "+1 123 456 7890",
+          linkedin: "foobar",
+          website: "http://foobar.com",
+          email: "someone@site.com",
+          location: {
+            country: "USA",
+            city: "New York",
+          }
         }
       }
     end
@@ -23,7 +34,7 @@ module Kamisaku
       invalid_content = @valid_content.merge(version: 2)
       validator = ContentValidator.new(content_hash: invalid_content)
 
-      error = assert_raises(Kamisaku::Error) { validator.validate! }
+      error = assert_raises(Error) { validator.validate! }
 
       assert_equal "Invalid version", error.message
     end
@@ -38,7 +49,7 @@ module Kamisaku
       invalid_content = @valid_content.except(:profile)
       validator = ContentValidator.new(content_hash: invalid_content)
 
-      error = assert_raises(Kamisaku::Error) { validator.validate! }
+      error = assert_raises(Error) { validator.validate! }
 
       assert_equal "Missing profile", error.message
     end
@@ -47,7 +58,7 @@ module Kamisaku
       invalid_content = @valid_content.merge(profile: "invalid_profile")
       validator = ContentValidator.new(content_hash: invalid_content)
 
-      error = assert_raises(Kamisaku::Error) { validator.validate! }
+      error = assert_raises(Error) { validator.validate! }
 
       assert_equal "Profile must be a hash", error.message
     end
@@ -57,7 +68,7 @@ module Kamisaku
       invalid_content = @valid_content.merge(profile: invalid_profile)
       validator = ContentValidator.new(content_hash: invalid_content)
 
-      error = assert_raises(Kamisaku::Error) { validator.validate! }
+      error = assert_raises(Error) { validator.validate! }
 
       assert_equal "Profile contains invalid fields", error.message
     end
@@ -67,7 +78,7 @@ module Kamisaku
       invalid_content = @valid_content.merge(profile: invalid_profile)
       validator = ContentValidator.new(content_hash: invalid_content)
 
-      error = assert_raises(Kamisaku::Error) { validator.validate! }
+      error = assert_raises(Error) { validator.validate! }
 
       assert_equal "Profile must contain exactly the fields: name, title, about", error.message
     end
@@ -77,10 +88,122 @@ module Kamisaku
       invalid_content = @valid_content.merge(profile: invalid_profile)
       validator = ContentValidator.new(content_hash: invalid_content)
 
-      error = assert_raises(Kamisaku::Error) { validator.validate! }
+      error = assert_raises(Error) { validator.validate! }
 
       assert_equal "Profile field 'about' must be a string", error.message
     end
 
+    def test_validate_contact_success
+      validator = ContentValidator.new(content_hash: @valid_content)
+
+      assert_silent { validator.validate! }
+    end
+
+    def test_validate_contact_missing
+      invalid_content = @valid_content.except(:contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Missing contact", error.message
+    end
+
+    def test_validate_contact_not_a_hash
+      invalid_content = @valid_content.merge(contact: "invalid_contact")
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Contact must be a hash", error.message
+    end
+
+    def test_validate_contact_invalid_fields
+      invalid_contact = @valid_content[:contact].merge(extra_field: "invalid")
+      invalid_content = @valid_content.merge(contact: invalid_contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Contact contains invalid fields", error.message
+    end
+
+    def test_validate_contact_missing_field
+      invalid_contact = @valid_content[:contact].except(:email)
+      invalid_content = @valid_content.merge(contact: invalid_contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Contact missing required field 'email'", error.message
+    end
+
+    def test_validate_contact_field_non_string_value
+      invalid_contact = @valid_content[:contact].merge(email: 123)
+      invalid_content = @valid_content.merge(contact: invalid_contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Contact field 'email' must be a string", error.message
+    end
+
+    def test_validate_contact_location_success
+      validator = ContentValidator.new(content_hash: @valid_content)
+
+      assert_silent { validator.validate! }
+    end
+
+    def test_validate_contact_location_missing
+      invalid_contact = @valid_content[:contact].except(:location)
+      invalid_content = @valid_content.merge(contact: invalid_contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Contact missing required field 'location'", error.message
+    end
+
+    def test_validate_contact_location_not_a_hash
+      invalid_contact = @valid_content[:contact].merge(location: "invalid_location")
+      invalid_content = @valid_content.merge(contact: invalid_contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Location must be a hash", error.message
+    end
+
+    def test_validate_contact_location_invalid_fields
+      invalid_location = @valid_content[:contact][:location].merge(extra_field: "invalid")
+      invalid_contact = @valid_content[:contact].merge(location: invalid_location)
+      invalid_content = @valid_content.merge(contact: invalid_contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Location contains invalid fields", error.message
+    end
+
+    def test_validate_contact_location_missing_field
+      invalid_location = @valid_content[:contact][:location].except(:city)
+      invalid_contact = @valid_content[:contact].merge(location: invalid_location)
+      invalid_content = @valid_content.merge(contact: invalid_contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Location missing required field 'city'", error.message
+    end
+
+    def test_validate_contact_location_field_non_string_value
+      invalid_location = @valid_content[:contact][:location].merge(city: 123)
+      invalid_contact = @valid_content[:contact].merge(location: invalid_location)
+      invalid_content = @valid_content.merge(contact: invalid_contact)
+      validator = ContentValidator.new(content_hash: invalid_content)
+
+      error = assert_raises(Error) { validator.validate! }
+
+      assert_equal "Location field 'city' must be a string", error.message
+    end
   end
 end
