2c3d97a451f3031fb1e362cc63714ecc904f4053
Sinaru Gunawardena <sinarug@gmail.com>
2025-06-06 00:41:20 +0100

    Fix script

--- scripts/rebuild_examples.rb
+++ scripts/rebuild_examples.rb
@@ -1,10 +1,34 @@
+
 require_relative '../lib/kamisaku'
 require 'fileutils'
+require 'optparse'
+
+# Parse command line arguments
+options = {}
+OptionParser.new do |opts|
+  opts.banner = "Usage: #{$0} [options]"
+
+  opts.on('-t', '--template TEMPLATE', 'Use specific template instead of all templates') do |template|
+    options[:template] = template
+  end
+
+  opts.on('-z', '--generate-html', 'Generate HTML files') do
+    options[:generate_html] = true
+  end
+
+  opts.on('-h', '--help', 'Show this help message') do
+    puts opts
+    exit
+  end
+end.parse!
 
 example_yaml_fie = File.expand_path('../lib/schema/example.yml', __dir__)
-yaml_str = yaml_str = File.read(example_yaml_fie)
+yaml_str = File.read(example_yaml_fie)
 
-Kamisaku::TemplateHelpers::TEMPLATES.each do |template|
+# Use specified template or all templates
+templates = options[:template] ? [options[:template]] : Kamisaku::TemplateHelpers::TEMPLATES
+
+templates.each do |template|
   template_dir = File.expand_path("../examples/#{template}", __dir__)
   FileUtils.mkdir_p(template_dir)
 
@@ -12,5 +36,11 @@ Kamisaku::TemplateHelpers::TEMPLATES.each do |template|
 
   content_hash = Kamisaku::Helpers.yaml_str_to_content_hash(yaml_str)
   pdf = Kamisaku::PDF.new(content_hash:, template:)
+  puts "Building template #{template}"
   pdf.write_to(pdf_file_path)
+
+  if options[:generate_html]
+    html_location = File.expand_path("../examples/#{template}/example.html", __dir__)
+    pdf.generate_html(html_location)
+  end
 end
