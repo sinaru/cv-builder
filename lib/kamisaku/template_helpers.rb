6f7cc537d84163d4a3ee9e835035b3583f3a6ce8
Sinaru Gunawardena <sinarug@gmail.com>
2025-06-05 23:25:56 +0100

    Fix up

--- lib/kamisaku/template_helpers.rb
+++ lib/kamisaku/template_helpers.rb
@@ -2,7 +2,7 @@ require "date"
 
 module Kamisaku
   module TemplateHelpers
-    TEMPLATES = %w[paper sleek bioline].freeze
+    TEMPLATES = %w[paper sleek].freeze
 
     def month_name(month_int)
       return "" if month_int.nil?
