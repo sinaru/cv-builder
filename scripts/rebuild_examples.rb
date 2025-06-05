4d10621d1e394a579a73caaf2e3f4f8764107d16
Sinaru Gunawardena <sinarug@gmail.com>
2025-06-06 00:19:52 +0100

    Add a script to regenerate PDFs for all the templates

---
+++ scripts/rebuild_examples.rb
@@ -0,0 +1,16 @@
+require_relative '../lib/kamisaku'
+require 'fileutils'
+
+example_yaml_fie = File.expand_path('../lib/schema/example.yml', __dir__)
+yaml_str = yaml_str = File.read(example_yaml_fie)
+
+Kamisaku::TemplateHelpers::TEMPLATES.each do |template|
+  template_dir = File.expand_path("../examples/#{template}", __dir__)
+  FileUtils.mkdir_p(template_dir)
+
+  pdf_file_path = File.expand_path("../examples/#{template}/example.pdf", __dir__)
+
+  content_hash = Kamisaku::Helpers.yaml_str_to_content_hash(yaml_str)
+  pdf = Kamisaku::PDF.new(content_hash:, template: "paper")
+  pdf.write_to(pdf_file_path)
+end
