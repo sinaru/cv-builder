acd63aa603af9085a4b840c3f134139356bb8788
Sinaru Gunawardena <sinarug@gmail.com>
2025-06-06 17:04:14 +0100

    Add new `meridian` template

--- lib/kamisaku/template_helpers.rb
+++ lib/kamisaku/template_helpers.rb
@@ -2,7 +2,7 @@ require "date"
 
 module Kamisaku
   module TemplateHelpers
-    TEMPLATES = %w[paper sleek zenith].freeze
+    TEMPLATES = %w[paper sleek zenith meridian].freeze
 
     def month_name(month_int)
       return "" if month_int.nil?
